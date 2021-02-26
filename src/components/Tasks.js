be69beee6ad449d32e8cfcc4f6615e38281d6cf2
jalonghurst <jacqui.longhurst@btinternet.com>
2021-02-26 15:30:18 +0000

    tasks component

---
+++ src/components/Tasks.js
@@ -0,0 +1,33 @@
+const tasks = [
+    {
+        id: 1,
+        text: 'Doctors',
+        day: 'Feb 5th',
+        reminder: true,
+    },
+
+    {
+        id: 2,
+        text: 'School',
+        day: 'Feb 12th',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Shopping',
+        day: 'Feb 15th',
+        reminder: false,
+    }
+
+]
+
+
+const Tasks = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Tasks
