d730f15d5c5201902c4739f422a079f4d5051625
jalonghurst <jacqui.longhurst@btinternet.com>
2021-02-26 14:34:44 +0000

    button event

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,10 @@
 import PropTypes from 'prop-types'
 
 
-const Button = ({color, text}) => {
-    return <button style= {{ backgroundColor: color}}
+const Button = ({color, text, onClick}) => {
+
+    return <button onClick={onClick} style= {{
+     backgroundColor: color}}
      className='btn' >{text}</button> 
     
     
@@ -15,5 +17,6 @@ Button.defaultProps = {
 Button.propTypes = {
     text: PropTypes.string,
     color: PropTypes.string,
+    onClick: PropTypes.func,
 }
 export default Button
